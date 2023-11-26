e3a9bb7883f49ec6b816c47ea39cfcefc8b051f5
Shakhzot2503 <mc.mashanlo@gmail.com>
2023-11-26 18:40:16 +0600

    instargam

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -16,7 +16,7 @@ export default function Footer() {
                     <div className="footer__wrapper">
                         <ul className="social">
                             <li className="social__item"><a href="#!"><img src={vk} alt="Link" /></a></li>
-                            <li className="social__item"><a href="#!"><img src={instagram} alt="Link" /></a></li>
+                            <li className="social__item"><a href="https://instagram.com/hht_di?igshid=NGVhN2U2NjQ0Yg=="><img src={instagram} alt="Link" /></a></li>
                             <li className="social__item"><a href="#!"><img src={twitter} alt="Link" /></a></li>
                             <li className="social__item"><a href="#!"><img src={gitHub} alt="Link" /></a></li>
                             <li className="social__item"><a href="#!"><img src={linkedIn} alt="Link" /></a></li>
