cd98b226c728c189ee8d90d7dbcd3aa3b6e9dc2e
Shakhzot2503 <mc.mashanlo@gmail.com>
2023-11-26 17:03:14 +0600

    вывава

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
            <Route path="/projects" element={<Projects />}/>
            <Route path="/project/:id" element={<Project />}/>
            <Route path="/contacts" element={<Contacts />}/>   
-        </Routes> 
+        </Routes>  
         <Footer />
       </Router>
     </div>
